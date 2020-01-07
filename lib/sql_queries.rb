edec405af8bf21b4918cc0325a44587e73812308
kelly r maass <kmaass77627@gmail.com>
2020-01-07 05:59:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,8 +30,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     HAVING over_goal >= 0;"
 end
 
-def selects_user_names_and_amounts_of_all_pledges_grouped
-  by_name_then_orders_them_by_the_summed_amount
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, 
   SUM(pledges.amount) 
   AS total_pledges 
