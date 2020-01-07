b3090dd58d515fbe2ea8ef5a90fcf7a092b54b7b
kelly r maass <kmaass77627@gmail.com>
2020-01-07 00:46:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,13 @@ def selects_user_names_and_amounts_of_all_pledges_grouped
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+    "SELECT projects.category, pledges.amount 
+    FROM projects 
+    INNER JOIN pledges 
+    ON projects.id = pledges.project_id 
+    WHERE category = ‘music’; "
+end
+
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
